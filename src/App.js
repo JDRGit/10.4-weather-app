848ed24ff2a405de6bb04a0cf27a770a19cc309f
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-02-09 15:41:56 -0500

    updated version - geolocation -working state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Weather from './components/Weather';
-export default function App() {
+import React from "react";
+import Weather from "./components/Weather";
 
-const [lat, setLat] = useState([]);
-const [long, setLong] = useState([]);
-const [data, setData] = useState([]);
-
-useEffect(() => {
-  const fetchData = async () => {
-  navigator.geolocation.getCurrentPosition((position) => {
-    setLat(position.coords.latitude);
-    setLong(position.coords.longitude);
-  });
-
-  await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result);
-      console.log(result);  
-    });
-  }
-  fetchData();
-}, [lat, long]);     
-
+const App = () => {
   return (
-    <div className="App">
-      {typeof data.main != "undefined" ? (
-        <Weather weatherData={data} />
-      ) : (
-        <div></div>
-      )}
-
+    <div>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
-}
+};
 
-
+export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,44 @@
-import React from "react";
-import { Card } from 'semantic-ui-react'
+import React, { useState, useEffect } from "react";
 
-const WeatherCard = ({ weatherData }) => (
-    <Card header={weatherData.name} />
-)
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState({});
+  const [error, setError] = useState("");
 
-export default WeatherCard;
+  const fetchWeatherData = async (lat, lon) => {
+    const API_KEY = "1a20232643563d73c8a804ad7de5825f";
+    const API_URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`;
+
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        fetchWeatherData(position.coords.latitude, position.coords.longitude);
+      },
+      (error) => setError(error.message)
+    );
+  }, []);
+
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      {weatherData.name && (
+        <div>
+          <h2>Weather in {weatherData.name}:</h2>
+          <p>Temperature: {weatherData.main.temp}°F</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Conditions: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
