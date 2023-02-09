4b135e6261a377ecc5bc1ca64f47f8116655d0a6
Jaime D. Rodriguez <118196254+JDRGit@users.noreply.github.com>
2023-02-09 09:59:57 -0500

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+export default function App() {
 
-function App() {
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  });
+
+  console.log("Latitude is:", lat)
+  console.log("Longitude is:", long)
+}, [lat, long]);
+
   return (
     <div className="App">
      
@@ -8,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+
